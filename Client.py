29052f089ab8503486fc305d4d6a7f2f0457026a
konstantinosTsoutsis <kostantinos.tsoutsis@gmail.com>
2021-08-21 12:51:19 +0300

    final

--- Client.py
+++ Client.py
@@ -5,34 +5,38 @@ import binascii
 #Host IP to send to.
 serverIP = '127.0.0.1'
 #Port to send to
-serverPort = 1000
-
+serverPort = 1100
 #create client socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #connect
 clientSocket.connect((serverIP, serverPort))
-
+msg_type = 0
+msg_id = 35
+msg_length = 13
 #user input numbers
 number1 = input("enter number 1 : \n")
-number1 = bytes(number1 , "utf-8")
 number2 = input("enter number 2 : \n")
-number2 = bytes(number2 , "utf-8")
 #user input the arithmetic symbol
 arithmeticSymbol = input("enter the arithmetic symbol :\nfor sum type '+'\nfor Difference type '-'\nfor Product type '*'\nfor Fraction type '/'\n")
-arithmeticSymbol = bytes(arithmeticSymbol , "utf-8")
+msg = pack('HHIHHx1s', msg_type , msg_length , msg_id ,int(number1) , int(number2) , arithmeticSymbol.encode('utf-8'))
 
-#send the numbers to server
-clientSocket.send(number1)
-clientSocket.send(number2)
-#send the arithmetic symbol
-clientSocket.send(arithmeticSymbol)
+#send
+clientSocket.sendall(msg)
 
-sum = clientSocket.recv(1024)
-# decode to unicode string 
-strings = sum.decode('utf8')
-#get the num
-num = float(strings)
-print("the sum is : " , num)
+#receve sym from server
+sum = clientSocket.recv(12)
+
+msg_type , response_code ,msg_id , sum = unpack('2HIf', sum)
+
+if response_code == 0 :
+    print("the sum is : " , sum)
+elif response_code == 1 :
+    print("Number is greater than 3000") 
+elif response_code == 2 :
+    print("Impossible division")
+elif response_code == 3 :
+    print("arithmetic symbol not found")    
+clientSocket.close()
 
 
 
--- Server.py
+++ Server.py
@@ -5,7 +5,7 @@ import binascii
 #Host IP to listen to. If '' then all IPs in this interface
 serverIP = '127.0.0.1'
 #Port to listen to
-serverPort = 1000
+serverPort = 1100
 close = False
 #create server socker
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket :
@@ -20,43 +20,44 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket :
         print("Connected by:", addr)
         print("Server Socket port: ", conn.getsockname())
         print("Client Socket port: ", conn.getpeername())
-        #recive the 2 numbers as bytes
-        number1 = conn.recv(1024)
-        number2 = conn.recv(1024)
-        #recive the arithmetic symbol
-        arithmeticSymbol = conn.recv(1024)
-        #convert bytes to string
-        number1 = number1.decode("utf-8")
-        number2 = number2.decode("utf-8")
-        arithmeticSymbol = arithmeticSymbol.decode("utf-8")
-        #convert string to int
+        msg = conn.recv(4)
+        msg_type1 , msg_length1 = unpack('HH', msg)
+        
+        msg = conn.recv(msg_length1 - 4 + 1)
+        msg_id , number1 ,number2 , arithmeticSymbol = unpack('IHHx1s', msg)
 
+        arithmeticSymbol = arithmeticSymbol.decode("utf-8")
         number1 = float(number1)
         number2 = float(number2)
-        if arithmeticSymbol == "+" :
-            sum = number1 + number2        
-        elif arithmeticSymbol == "-" :
-            sum = number1 - number2
-        elif arithmeticSymbol == "*" :
-            sum = number1 * number2
-        elif arithmeticSymbol == "/" :
-            if number2 == 0 : 
-                sum = str(sum)
-                sum = "error . you cant divine a number with 0"
-            else :
-                sum = number1 / number2
-        else :
-            print("arithmetic symbol dont found")
+        
+        
+        response_code = 0
+        sum = 0
         if (number1 >= 0 and  number1 <= 30000) and  (number2 >= 0 and  number2 <= 30000):
-            #convert sum to string and then encode to bytes
-            #send sum to client
-            sum = str(sum)
-            conn.send(str(sum).encode("utf-8"))        
-            conn.close()
+            if arithmeticSymbol == "+" :
+              sum = number1 + number2        
+            elif arithmeticSymbol == "-" :
+              sum = number1 - number2
+            elif arithmeticSymbol == "*" :
+              sum = number1 * number2
+            elif arithmeticSymbol == "/" :
+                if number2 == 0 : 
+                    response_code = 2   
+                else :
+                    sum = number1 / number2
+            else :
+                response_code = 3
         else :
-            sum = "error . must type a number between 0 and 3000"
-            conn.send(str(sum).encode("utf-8"))        
-            conn.close()
+            response_code == 1            
+       
+       
+
+        msg_type1 = 1
+        msg = pack('HHIf', msg_type1 , response_code, msg_id , sum)
+        conn.sendall(msg)
+        
+        serverSocket.close()
+        
                 
                 
 
